4c84b7da2b864b1eeaf0f115c4073a0ce291e58e
Olusesan <olusesan.ajina@gmail.com>
2015-02-20 16:29:22 -0500

    Additional Movie Cell configuration

---
+++ RottenTomatoes/moviecell.h
@@ -0,0 +1,13 @@
+//
+//  Moviecell.h
+//  RottenTomatoes
+//
+//  Created by Olusesan Ajina on 2/18/15.
+//  Copyright (c) 2015 Olusesan Ajina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Moviecell : UIViewController
+
+@end
